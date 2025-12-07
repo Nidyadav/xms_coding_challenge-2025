0700ff15dbf3f278d118193afa29dcd5a199d17a
Nidhi <xs2nidhi@gmail.com>
2025-12-07 20:58:51 +0100

    day7: maxBlock countreturn the max count of same adjacent block of characters

--- src/CodingBatStringProblems.java
+++ src/CodingBatStringProblems.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class CodingBatStringProblems {
     public String firstHalf(String str) {
 
@@ -7,6 +9,25 @@ public class CodingBatStringProblems {
           stringBuilder.append(charArray[i]);
       }
       return  stringBuilder.toString();
+    }
+    /*
+    Given a string, return the length of the largest "block" in the string. A block is a run of "adjacent chars" that are the same.
+    maxBlock("hoopla") → 2
+    maxBlock("abbCCCddBBBxx") → 3
+    maxBlock("") → 0
+     */
+    public int maxBlock(String str) {
+       int currentCount=1;
+       int maxCount=1;
+       for(int i=1;i<str.length();i++){
+          if(str.charAt(i)==str.charAt(i-1)){
+              currentCount++;
+          }else{
+              currentCount=1;
+          }
+           maxCount = Math.max(maxCount, currentCount);
+       }
+      return maxCount;
     }
 
 }
