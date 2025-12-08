76279d4345174e2d6d5e8b30574908623ef96aaf
Nidhi <xs2nidhi@gmail.com>
2025-12-08 19:43:49 +0100

    Day 8: return the sum of digits in a string

--- src/CodingBatStringProblems.java
+++ src/CodingBatStringProblems.java
@@ -29,5 +29,19 @@ public class CodingBatStringProblems {
        }
       return maxCount;
     }
+    /*
+    Given a string, return the sum of the digits 0-9 that appear in the string, ignoring all other characters. Return 0 if there are no digits in the string. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+     */
+    public int sumDigits(String str) {
+      char[] charArray= str.toCharArray();
+      int sum=0;
+      for(char c:charArray){
+          if(Character.isDigit(c)){
+              sum+=Integer.parseInt(String.valueOf(c));
+          }
+      }
+      return sum;
+    }
+
 
 }
