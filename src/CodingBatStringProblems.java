10b56fba50d862fbc050419051ce0ba482aac435
Nidhi <xs2nidhi@gmail.com>
2025-12-10 18:27:43 +0100

    Day 10:: MirrorEnds in string

--- src/CodingBatStringProblems.java
+++ src/CodingBatStringProblems.java
@@ -60,6 +60,21 @@ public class CodingBatStringProblems {
         }
         return countOfIs == countOfNot;
     }
-
-
+    /*
+    Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string. In other words, zero or more characters at the very begining of the given string, and at the very end of the string in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab".
+    mirrorEnds("abXYZba") → "ab"
+    mirrorEnds("abca") → "a"
+    mirrorEnds("aba") → "aba"
+     */
+    public String mirrorEnds(String string) {
+        StringBuilder mirrorString = new StringBuilder();
+        for (int i = string.length() - 1; i >= 0; i--) {
+            if (string.charAt(i) == string.charAt(string.length() - i - 1)) {
+                mirrorString.append(string.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return mirrorString.toString();
+    }
 }
