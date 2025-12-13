cd44ece78eb4f56c76c921007b6ef95dc08cca82
Nidhi <xs2nidhi@gmail.com>
2025-12-13 21:54:13 +0100

    Day 13: use functional programming to return a list of string that filter out string that contains z

---
+++ src/CodingBatFunctionalProblems.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CodingBatFunctionalProblems {
+    /*
+    Given a list of strings, return a list of the strings, omitting any string that contains a "z". (Note: the str.contains(x) method returns a boolean)
+    noZ(["aaa", "bbb", "aza"]) → ["aaa", "bbb"]
+    noZ(["hziz", "hzello", "hi"]) → ["hi"]
+    noZ(["hello", "howz", "are", "youz"]) → ["hello", "are"]
+     */
+    public List<String> noz(List<String> strings) {
+        return strings.stream()
+                .filter(s->s.contains("z"))
+                .collect(Collectors.toList());
+    }
+
+}
