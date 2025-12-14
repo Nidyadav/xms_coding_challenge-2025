d49e0c7ea558b9edd9b2264fb4291f2ade140e20
Nidhi <xs2nidhi@gmail.com>
2025-12-14 20:52:33 +0100

    Day 14: Functional programming:return a list where each string has 'y' added at its end, omitting any resulting strings that contain 'YY'

--- src/CodingBatFunctionalProblems.java
+++ src/CodingBatFunctionalProblems.java
@@ -13,4 +13,15 @@ public class CodingBatFunctionalProblems {
                 .filter(s->!s.contains("z"))
                 .collect(Collectors.toList());
     }
+    /*Given a list of strings, return a list where each string has "y" added at its end, omitting any resulting strings that contain "yy" as a substring anywhere.
+    noYY(["a", "b", "c"]) → ["ay", "by", "cy"]
+    noYY(["a", "b", "cy"]) → ["ay", "by"]
+    noYY(["xx", "ya", "zz"]) → ["xxy", "yay", "zzy"]*/
+    public List<String> noYY(List<String> strings) {
+        return strings.stream()
+                .map(str -> str + "y")
+                .filter(str -> !str.contains("yy"))
+                .collect(Collectors.toList());
+    }
+
 }
