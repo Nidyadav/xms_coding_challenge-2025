36d8d11260be177a57da778d12537c41a4b2ae5b
Nidhi <xs2nidhi@gmail.com>
2025-12-15 17:03:16 +0100

    Day 15:filter numbers between 13 and 19

--- src/CodingBatFunctionalProblems.java
+++ src/CodingBatFunctionalProblems.java
@@ -23,5 +23,13 @@ public class CodingBatFunctionalProblems {
                 .filter(str -> !str.contains("yy"))
                 .collect(Collectors.toList());
     }
+    /*
+Given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive.
+*/
+    public List<Integer> noTeen(List<Integer> nums) {
+      return nums.stream()
+              .filter(n-> n < 13 || n > 19)
+              .collect(Collectors.toList());
+    }
 
 }
